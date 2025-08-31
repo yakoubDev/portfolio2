793718445cba1c5ba29e8cb0da9476c495e01c79
Your Name <ahcenetayebi@gmail.com>
2025-08-31 20:27:23 +0100

    Removed pulses circles

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,11 +27,11 @@ export default function RootLayout({
         <Nav />
         <div className="mt-30">
           {/* Animated background elements */}
-          <div className="absolute inset-0 overflow-hidden">
+          {/* <div className="absolute inset-0 overflow-hidden">
             <div className="absolute -top-40 -right-40 w-80 h-80 bg-cyan-400/20 rounded-full blur-3xl animate-pulse"></div>
             <div className="absolute top-40 -left-40 w-60 h-60 bg-blue-500/20 rounded-full blur-3xl animate-pulse delay-1000"></div>
             <div className="absolute bottom-20 right-20 w-40 h-40 bg-purple-500/20 rounded-full blur-2xl animate-pulse delay-2000"></div>
-          </div>
+          </div> */}
           {children}
         </div>
         <Footer />
